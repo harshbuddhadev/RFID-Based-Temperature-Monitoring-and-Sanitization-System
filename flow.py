f7ed90e4f10ccb6f6b27b8759dd1fe02e189347e
harsh.buddhadev@omen15 <buddhadevharsh953@gmail.com>
2023-06-30 21:17:09 +0530

    Updated flow.py

--- flow.py
+++ flow.py
@@ -1,23 +1,32 @@
+## Import Libraries
 import os
-from cam import capture
-import cam
-from upload import sheetsupdate
-from get_temp import temp
-import RPi.GPIO as GPIO
-GPIO.cleanup()
-GPIO.setmode(GPIO.BCM)
 import time
+import RPi.GPIO as GPIO
 from gpiozero import Servo
 import pigpio
-os.popen("sudo pigpiod") #### AGAR NHI CHALA TOH PUT THIS COMMAND MANUALLY ####
-from validate import vali
 import busio
 from PIL import Image, ImageDraw, ImageFont
 import adafruit_ssd1306
+## Import Libraries
 
 
+## Import Custom Libraries ##
+from cam import capture
+import cam
+from get_temp import temp
+from validate import vali
+from upload import sheetsupdate
+## Import Custom Libraries ##
 
-###############################OLED_INIT###############################
+## INIT Commands ##
+GPIO.cleanup()
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+os.popen("sudo pigpiod") #Init Pi GPIO
+os.popen("sudo stty -F /dev/ttyUSB0 9600 time 2 min 100 -hupcl brkint ignpar -icrnl -opost -onlcr -isig -icanon -echo") #Init Serial Port
+## INIT Commands ##
+
+## OLED_INIT ##
 i2c = busio.I2C(3, 2)
 disp = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c)
 disp.fill(0)
@@ -32,180 +41,170 @@ top = padding
 bottom = height - padding
 x = 0
 font = ImageFont.load_default()
-draw.text((x, top + 0), "      TEAM LAZR", font=font, fill=255)
+draw.text((x, top + 0), "	  TEAM LAZR", font=font, fill=255)
 disp.show()
-###############################OLED_INIT###############################
+## OLED_INIT ##
 
 
-
-
-os.popen("sudo stty -F /dev/ttyUSB0 9600 time 2 min 100 -hupcl brkint ignpar -icrnl -opost -onlcr -isig -icanon -echo")
-# os.popen("sudo pigpiod")
-
-
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-
+## Define Pins ##
 relay=21
 buzzer=5
 servo = 22
 red_led=13
 green_led=6
+## Define Pins ##
 
+## Init GPIO ##
 GPIO.setup(buzzer,GPIO.OUT)
 GPIO.setup(red_led,GPIO.OUT)
 GPIO.setup(green_led,GPIO.OUT)
 pwm = pigpio.pi() 
 pwm.set_mode(servo, pigpio.OUTPUT)
 pwm.set_PWM_frequency( servo, 50 )
-htemp=40.0
-ltemp=30.0
+## Init GPIO ##
+
+## Init Threshold ##
+htemp=37.78
+ltemp=32.00
+## Init Threshold ##
+
+id=[] ## Init Empty Lists for Data
 
 
-id=[]
 
 
-
-
-##################################TAGSTART###################################
+## TAGSTART ##
 try:
-    while True:
-        print("Place your Card On the RFID Scanner")
-        draw.rectangle((0, 0, width, height), outline=0, fill=0)
-        draw.text((x, top + 0), "      TEAM LAZR", font=font, fill=255)
-        draw.text((x, top + 8), "Place your Card On", font=font, fill=255)
-        draw.text((x, top + 16), " the RFID Scanner", font=font, fill=255)
-        disp.image(image)
-        disp.show()
-        tag_var=str(os.popen("sudo head -c 12 /dev/ttyUSB0").read())
-        capture()
-        GPIO.output(green_led,GPIO.HIGH)
-        time.sleep(0.75)
-        GPIO.output(green_led,GPIO.LOW)
-        print(tag_var)
-        tag_valid=False
-        id=vali(tag_var)
-        if(len(id) > 0):
-            tag_valid=True
-            print(id)
-            print("a")
-            draw.rectangle((0, 0, width, height), outline=0, fill=0)
-            draw.text((x, top + 0), "      TEAM LAZR", font=font, fill=255)
-            draw.text((x, top + 8), "FName: "+id[0], font=font, fill=255)
-            draw.text((x, top + 16), "LName: "+id[1], font=font, fill=255)
-            draw.text((x, top + 25), "SID : "+id[2], font=font, fill=255)
-            disp.image(image)
-            disp.show()
-        elif(len(id) == 0 ):
-            print("updating sheets")
-            sheetsupdate("-","-","-",tag_var,"-","Invalid Card")
-            GPIO.output(green_led,GPIO.LOW)
-            print("both false")
-            for i in range(10):
-                GPIO.output(buzzer,GPIO.HIGH)
-                GPIO.output(red_led,GPIO.HIGH)
-                time.sleep(0.2)
-                GPIO.output(buzzer,GPIO.LOW)
-                GPIO.output(red_led,GPIO.LOW)
-                time.sleep(0.2)
-        
+	while True:
+		print("Place your Card On the RFID Scanner")
 
-    
-    
-##################################TAGEND###################################
-    
+		draw.rectangle((0, 0, width, height), outline=0, fill=0)
+		draw.text((x, top + 0), "	  TEAM LAZR", font=font, fill=255)
+		draw.text((x, top + 8), "Place your Card On", font=font, fill=255)
+		draw.text((x, top + 16), " the RFID Scanner", font=font, fill=255)
+		disp.image(image)
+		disp.show()
+		
+		tag_var=str(os.popen("sudo head -c 12 /dev/ttyUSB0").read()) #Read RFID Tag
+		capture() # Capture Image
 
-        temp_var=False
-        door_on=False
-##################################TEMPSTART###################################
-        if(tag_valid):
-            print ("Place Your finger on the temprature sensor")
-            time.sleep(3)
-            bodytemp=temp()
-            if(bodytemp>htemp):
-                print("High")
-                for i in range(10):
-                    GPIO.output(buzzer,GPIO.HIGH)
-                    GPIO.output(red_led,GPIO.HIGH)
-                    time.sleep(0.2)
-                    GPIO.output(buzzer,GPIO.LOW)
-                    GPIO.output(red_led,GPIO.LOW)
-                    time.sleep(0.2)
-                
-                print("updating sheets")
-                sheetsupdate(id[0],id[1],id[2],tag_var,bodytemp,"High Temp")
-            elif(bodytemp<=htemp and bodytemp>=ltemp):
-                print("Normal")
-                temp_var=True
-                GPIO.output(buzzer,GPIO.HIGH)
-                time.sleep(1)
-                GPIO.output(buzzer,GPIO.LOW)
-                print("updating sheets")
-                sheetsupdate(id[0],id[1],id[2],tag_var,bodytemp,"Good Temp")
+		GPIO.output(green_led,GPIO.HIGH)
+		time.sleep(0.75)
+		GPIO.output(green_led,GPIO.LOW)
+		
+		print(tag_var)
+		tag_valid=False
+		id=vali(tag_var) #Validate RFID Tag
+		
+		if(len(id) > 0): #If ID in List
+			tag_valid=True
+			print(id)
 
-                    
-            elif(bodytemp<ltemp):
-                print("Could Not Detect Temperature Try Again!!!")
-                for i in range(3):
-                    GPIO.output(buzzer,GPIO.HIGH)
-                    GPIO.output(red_led,GPIO.HIGH)
-                    time.sleep(1)
-                    GPIO.output(buzzer,GPIO.LOW)
-                    GPIO.output(red_led,GPIO.LOW)
-                    time.sleep(1)
-                print("updating sheets")
-                sheetsupdate(id[0],id[1],id[2],tag_var,bodytemp,"No Temp")
-            
-
-                
-##################################TEMPEND###################################
+			draw.rectangle((0, 0, width, height), outline=0, fill=0)
+			draw.text((x, top + 0), "	  TEAM LAZR", font=font, fill=255)
+			draw.text((x, top + 8), "FName: "+id[0], font=font, fill=255)
+			draw.text((x, top + 16), "LName: "+id[1], font=font, fill=255)
+			draw.text((x, top + 25), "SID : "+id[2], font=font, fill=255)
+			disp.image(image)
+			disp.show()
+		
+		elif(len(id) == 0 ): # IF ID Not in List
+			print("updating sheets")
+			sheetsupdate("-","-","-",tag_var,"-","Invalid Card") #Update Google Sheet
+			GPIO.output(green_led,GPIO.LOW)
+			for i in range(10):
+				GPIO.output(buzzer,GPIO.HIGH)
+				GPIO.output(red_led,GPIO.HIGH)
+				time.sleep(0.2)
+				GPIO.output(buzzer,GPIO.LOW)
+				GPIO.output(red_led,GPIO.LOW)
+				time.sleep(0.2)
+		temp_var=False
+		door_on=False
+## TAGEND ##
 
 
-##################################DISPENSE START###################################
-
-        if(temp_var):
-            print("Temperature: ",bodytemp)
-            GPIO.output(green_led,GPIO.HIGH)
-            GPIO.setup(relay,GPIO.OUT)
-            print("Place your hand below the dispenser")
-            time.sleep(3)
-            GPIO.output(relay,GPIO.HIGH)
-            time.sleep(1)
-            GPIO.output(relay,GPIO.LOW)
-            time.sleep(0.3)
-            GPIO.output(relay,GPIO.HIGH)
-            door_on=True
-            GPIO.output(green_led,GPIO.LOW)
-
-##################################DISPENSEEND###################################
-
-
-##################################DOORSTART###################################
-
-        if(door_on):
-            GPIO.output(green_led,GPIO.HIGH)
-            print("Gate Open")
-            time.sleep(1.5)
-            pwm.set_servo_pulsewidth( servo, 500 ) ;#0 degree
-            time.sleep(0.5)
-            pwm.set_servo_pulsewidth( servo, 1500 ) ;#90 degree
-            time.sleep(3.75)
-            pwm.set_servo_pulsewidth( servo, 500 ) ;#0 degree
-            GPIO.output(green_led,GPIO.LOW)
-       
-##################################DOOREND###################################
+## TEMPSTART ##
+		if(tag_valid):
+			print ("Place Your finger on the temprature sensor")
+			time.sleep(3)
+			bodytemp=temp() #Fetch Temperature
+			if(bodytemp>htemp): #If Body Temp Higher Than Threshold
+				print("High Body Temp")
+				for i in range(10): #Ring Buzzer
+					GPIO.output(buzzer,GPIO.HIGH)
+					GPIO.output(red_led,GPIO.HIGH)
+					time.sleep(0.2)
+					GPIO.output(buzzer,GPIO.LOW)
+					GPIO.output(red_led,GPIO.LOW)
+					time.sleep(0.2)
+				
+				sheetsupdate(id[0],id[1],id[2],tag_var,bodytemp,"High Temp") #Update Google Sheet
+			elif(bodytemp<=htemp and bodytemp>=ltemp): #If Temperature within Threshold
+				print("Normal Body Temperature")
+				temp_var=True
+				GPIO.output(buzzer,GPIO.HIGH)
+				time.sleep(1)
+				GPIO.output(buzzer,GPIO.LOW)
+				sheetsupdate(id[0],id[1],id[2],tag_var,bodytemp,"Good Temp") #Update Google Sheet
 
 
-# Reset by pressing CTRL + C
+			elif(bodytemp<ltemp): #If Body Temperature below Threshold
+				print("Could Not Detect Temperature Try Again!!!")
+				for i in range(3): #Ring Buzzer
+					GPIO.output(buzzer,GPIO.HIGH)
+					GPIO.output(red_led,GPIO.HIGH)
+					time.sleep(1)
+					GPIO.output(buzzer,GPIO.LOW)
+					GPIO.output(red_led,GPIO.LOW)
+					time.sleep(1)
+				sheetsupdate(id[0],id[1],id[2],tag_var,bodytemp,"No Temp") #Update Google Sheet
+			
+
+				
+## TEMPEND ##
+
+
+## DISPENSE START ###
+
+		if(temp_var): #If Body Temp within Threshold, dispense sanitizer
+			print("Temperature: ",bodytemp)
+			GPIO.output(green_led,GPIO.HIGH)
+			GPIO.setup(relay,GPIO.OUT)
+			print("Place your hand below the dispenser")
+			time.sleep(3)
+			GPIO.output(relay,GPIO.HIGH)
+			time.sleep(1)
+			GPIO.output(relay,GPIO.LOW)
+			time.sleep(0.3)
+			GPIO.output(relay,GPIO.HIGH)
+			door_on=True
+			GPIO.output(green_led,GPIO.LOW)
+
+## DISPENSE END ##
+
+
+## DOORSTART ##
+
+		if(door_on):#If Body Temp within Threshold, Open Door
+			GPIO.output(green_led,GPIO.HIGH)
+			print("Gate Open")
+			time.sleep(1.5)
+			pwm.set_servo_pulsewidth( servo, 500 ) ;#0 degree
+			time.sleep(0.5)
+			pwm.set_servo_pulsewidth( servo, 1500 ) ;#90 degree
+			time.sleep(3.75)
+			pwm.set_servo_pulsewidth( servo, 500 ) ;#0 degree
+			GPIO.output(green_led,GPIO.LOW)
+	   
+## DOOREND ##
+
 except KeyboardInterrupt:
-    cam.capture()
-    print("Code stopped by User")
-    GPIO.cleanup()
-    print("GPIO Cleanup Called!!")
-    cam.camera.close()
-    
-
-
-
-
-
+	print("Code stopped by User")
+	cam.capture()
+	GPIO.cleanup()
+	print("GPIO Cleanup Called!!")
+	cam.camera.close()
+	print("Camera Closed!")
+	if __name__ == "__main__":
+		exit()
